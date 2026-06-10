4ee6d70236b741e6a648d880e29f647dc53d23de
abhiji-th <abhijithc366@gmail.com>
2026-06-10 22:03:01 +0530

    Created vector store

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 video_id = "Gfr50f6ZBvo"
 yt_api = YouTubeTranscriptApi()
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2"
+)
 
 try:
     transcript_list = yt_api.fetch(video_id)
@@ -10,7 +18,17 @@ try:
 except TranscriptsDisabled:
     print("Transcript not available for this video")
 
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=400)
 chunks = splitter.create_documents([transcript])
 
-print(len(chunks))
+# vectorstore = FAISS.from_documents(chunks, embeddings)
+
+# vectorstore.save_local("faiss_index")
+
+vectorstore = FAISS.load_local(
+    "faiss_index",
+    embeddings,
+    allow_dangerous_deserialization=True
+)
+
+print(vectorstore.index_to_docstore_id)
