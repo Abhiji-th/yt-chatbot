a17d8ee263ddeafe1943c97501e6a3db4baca6b3
abhiji-th <abhijithc366@gmail.com>
2026-06-10 21:25:33 +0530

    Added text splitting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 
 video_id = "Gfr50f6ZBvo"
 yt_api = YouTubeTranscriptApi()
@@ -8,3 +9,8 @@ try:
     transcript = " ".join(chunk.text for chunk in transcript_list)
 except TranscriptsDisabled:
     print("Transcript not available for this video")
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = splitter.create_documents([transcript])
+
+print(len(chunks))
